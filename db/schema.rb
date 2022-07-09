6b24f794e71c12fa43b0673aeeec67a48a6f8689
tentuku10 <sasaki.zuozuomu@gmail.com>
2022-07-09 14:16:27 +0900

    like_postモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_09_045738) do
+ActiveRecord::Schema.define(version: 2022_07_09_050606) do
+
+  create_table "like_posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "\"user_id\", \"board_id\"", name: "index_like_posts_on_user_id_and_board_id", unique: true
+    t.index ["post_id"], name: "index_like_posts_on_post_id"
+    t.index ["user_id"], name: "index_like_posts_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -30,5 +40,7 @@ ActiveRecord::Schema.define(version: 2022_07_09_045738) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "like_posts", "posts"
+  add_foreign_key "like_posts", "users"
   add_foreign_key "posts", "users"
 end
